0b7b1268c90a3457f09dd091bb525f8839dbac97
Alec German <kimarimt@protonmail.com>
2022-06-01 22:56:35 -0400

    added drawer for the net and scoreboard

--- main.py
+++ main.py
@@ -1,6 +1,46 @@
-def main():
-    print('Hello, World!')
+import turtle
+
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 500
+
+screen = turtle.getscreen()
+screen.title('Pong Game')
+screen.setup(SCREEN_WIDTH, SCREEN_HEIGHT)
+screen.bgcolor('black')
+
+drawer = turtle.Turtle()
+drawer.hideturtle()
+drawer.penup()
+drawer.color('white')
+drawer.setheading(270)
+drawer.speed(0)
+drawer.goto(0, 240)
+
+
+def draw_net():
+    while drawer.ycor() > -240:
+        drawer.pendown()
+        drawer.forward(15)
+        drawer.penup()
+        drawer.forward(5)
+
+
+score = 0
+
+
+def draw_scoreboard():
+    drawer.goto((-30, 180))
+    drawer.write(f'{score}', align='center', font=('Arial', 52, 'normal'))
+    drawer.goto((30, 180))
+    drawer.write(f'{score}', align='center', font=('Arial', 52, 'normal'))
 
 
 if __name__ == '__main__':
-    main()
+    is_running = True
+
+    draw_net()
+    draw_scoreboard()
+
+    while is_running:
+        screen.update()
