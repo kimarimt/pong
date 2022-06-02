d5123f3a1ea5c27a7ca89e3f4bb7d0e45564c0f4
Alec German <kimarimt@protonmail.com>
2022-06-02 00:11:38 -0400

    added a ball to game

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from calendar import c
+from os import scandir
 import turtle
 from drawer import Drawer
 from paddle import Paddle
@@ -14,18 +14,41 @@ screen.bgcolor('black')
 
 screen.tracer(False)
 drawer = Drawer()
-player = Paddle(coords=(-380, 0))
-computer = Paddle(coords=(370, 0))
+player_1 = Paddle(coords=(-380, 0))
+player_2 = Paddle(coords=(370, 0))
 screen.tracer(True)
 
+x_move = -10
+y_move = 10
+ball = turtle.Turtle()
+ball.penup()
+ball.color('white')
+ball.shape('circle')
+ball.shapesize(0.5)
+ball.speed(0)
+
+def move():
+    x = ball.xcor() + x_move
+    y = ball.ycor() + y_move
+    screen.tracer(False)
+    ball.goto(x, y)
+    screen.tracer(True)
+
+def bounce():
+    global y_move
+    if ball.ycor() > 230:
+        y_move *= -1
 
 if __name__ == '__main__':
     is_running = True
 
-    screen.onkey(player.go_up, 'w')
-    screen.onkey(player.go_down, 's')
+    screen.onkeypress(player_1.go_up, 'w')
+    screen.onkeypress(player_1.go_down, 's')
+    screen.onkeypress(player_2.go_up, 'Up')
+    screen.onkeypress(player_2.go_down, 'Down')
     screen.listen()
-
+    
     while is_running:
         screen.update()
-        computer.move()
+        move()
+        bounce()
--- paddle.py
+++ paddle.py
@@ -3,29 +3,23 @@ from turtle import Turtle
 
 class Paddle(Turtle):
 
-    def __init__(self, is_computer, coords):
+    def __init__(self, coords):
         super().__init__()
-        self.is_computer = is_computer
+        self.coords = coords
         self.penup()
         self.color('white')
         self.shape('square')
         self.shapesize(stretch_wid=1, stretch_len=3)
+        self.speed(0)
         self.setheading(90)
-        self.goto(coords)
+        self.goto(self.coords)
 
     def go_up(self):
         if self.ycor() < 215:
-            self.forward(10)
+            y = self.ycor() + 10
+            self.goto(self.coords[0], y)
 
     def go_down(self):
         if self.ycor() > -210:
-            self.backward(10)
-
-    def move(self):
-        if self.is_computer:
-            if self.ycor() < 215:
-                while self.ycor() < 215:
-                    self.forward(5)
-            else:
-                while self.ycor() > -210:
-                    self.backward(5)
+            y = self.ycor() - 10
+            self.goto(self.coords[0], y)
