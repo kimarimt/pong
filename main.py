e5501df6e140d6187a3ce1da1abd7fff45b42c85
Alec German <kimarimt@protonmail.com>
2022-06-01 23:26:22 -0400

    added both paddles to the screen

--- main.py
+++ main.py
@@ -14,9 +14,41 @@ screen.tracer(False)
 drawer = Drawer()
 screen.tracer(True)
 
+paddle = turtle.Turtle()
+paddle.penup()
+paddle.color('white')
+paddle.shape('square')
+paddle.shapesize(stretch_wid=1, stretch_len=3)
+paddle.setheading(90)
+paddle.goto(-380, 0)
+
+def go_up():
+    if paddle.ycor() < 215:
+        paddle.forward(10)
+
+def go_down():
+    if paddle.ycor() > -210:
+        paddle.backward(10)
+
+computer = paddle.clone()
+computer.goto(370, 0)
+
+def move():
+    if computer.ycor() < 215:
+        while computer.ycor() < 215:
+            computer.forward(5) 
+    else:
+        while computer.ycor() > -210:
+            computer.backward(5)
+
 
 if __name__ == '__main__':
     is_running = True
+    
+    screen.onkey(go_up, 'w')
+    screen.onkey(go_down, 's')
+    screen.listen()
 
     while is_running:
         screen.update()
+        move()
